49dcc293f838805bb24bb9bfb2615644e7ffc403
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2023-09-03 20:58:29 -0300

    style: stylesheet

--- App.tsx
+++ App.tsx
@@ -1,31 +1,23 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#a597af",
-        padding: 28,
-      }}
-    >
-      <Text
-        style={{
-          color: "#ffffff",
-          fontSize: 24,
-          fontWeight: "bold",
-          marginTop: 48,
-        }}
-      >
-        Nome do evento
-      </Text>
-      <Text
-        style={{
-          color: "#6b6b6b",
-          fontSize: 16,
-        }}
-      >
-        Domingo, 03 de Setembro de 2023{" "}
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Domingo, 03 de Setembro de 2023 </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "#a597af", padding: 28 },
+  eventName: {
+    color: "#ffffff",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: {
+    color: "#6b6b6b",
+    fontSize: 16,
+  },
+});

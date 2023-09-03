52a05658cdbace448b863cd3a4c65157ced6893b
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2023-09-03 20:41:34 -0300

    feat: app and style

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import { Text, View } from "react-native";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>testeee </Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#a597af",
+        padding: 28,
+      }}
+    >
+      <Text
+        style={{
+          color: "#ffffff",
+          fontSize: 24,
+          fontWeight: "bold",
+          marginTop: 48,
+        }}
+      >
+        Nome do evento
+      </Text>
+      <Text
+        style={{
+          color: "#6b6b6b",
+          fontSize: 16,
+        }}
+      >
+        Domingo, 03 de Setembro de 2023{" "}
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});

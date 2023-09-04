a0a2b1b24b793546769a89f13c813766f90a340a
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2023-09-03 21:07:27 -0300

    docs: estrutura de pastas

--- App.tsx
+++ App.tsx
@@ -1,23 +1,4 @@
-import { Text, View, StyleSheet } from "react-native";
+import Home from "./src/screens/Home";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
-      <Text style={styles.eventDate}>Domingo, 03 de Setembro de 2023 </Text>
-    </View>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "#a597af", padding: 28 },
-  eventName: {
-    color: "#ffffff",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 48,
-  },
-  eventDate: {
-    color: "#6b6b6b",
-    fontSize: 16,
-  },
-});
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "#a597af", padding: 28 },
+  eventName: {
+    color: "#ffffff",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: {
+    color: "#6b6b6b",
+    fontSize: 16,
+  },
+});

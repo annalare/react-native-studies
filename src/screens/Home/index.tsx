4b1003cfa061d51b8ea17606606326aa0344e2e9
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2023-09-03 21:24:12 -0300

    feat: text input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,15 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput } from "react-native";
 import { styles } from "./styles";
 export default function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Domingo, 03 de Setembro de 2023 </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6b6b6b"
+      />
     </View>
   );
 }
